71b821732fd826fd6df852f5abf0b48729d493c2
Ratul Nandy <ratul.tester@gmail.com>
2025-07-03 11:54:11 +0530

    MadJVM file added

---
+++ src/MadJVM.java
@@ -0,0 +1,10 @@
+public class MadJVM {
+    public static void main(String[] args) {
+
+        for(byte b= -128; b <= 126; b++){
+            System.out.print(b);
+            b++;
+            System.out.println(b);
+        }
+    }
+}
