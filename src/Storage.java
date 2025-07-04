399ed41da043f8c7a87eff14cda4c0e19d32de8f
Ratul Nandy <ratul.tester@gmail.com>
2025-07-04 16:35:19 +0530

    file added

---
+++ src/Storage.java
@@ -0,0 +1,27 @@
+public class Storage {
+    public static void main(String[] args) {
+        byte age = 123;
+        long mob_no = 8895566483l;
+        float pi = 3.145f;
+        double d = 3.145278951212121212121221245458799565653232323232212145454512121245454533546898787454545154878798979778787878787878787545454543213213454654646546546546465464687987987989897897949084984981484878977878946546516551546547878787878978978465465434354548484848487879465461312398989898989856656598989889898989987987987987878465465468686898955616318948979877987984654651321321657987987987987987879879879878;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char grade = 'A';
+        char grade1 = '1';
+
+        System.out.println("My age is "+age);
+        System.out.printf("My age is %d \n", age);
+        System.out.println("And my mobile no. is "+mob_no);
+        System.out.printf("And my mobile no. is %d \n", mob_no);
+        System.out.printf("The value of pi is %f \n", pi);
+        System.out.println("The value of pi is " + pi);
+        System.out.printf("The value of double is %f \n", d);
+        System.out.println("The value of double is" + d);
+        System.out.printf("The Grade value is %c \n", grade);
+        System.out.println("The Grade value is "+ grade);
+        System.out.printf("The Grade1 value is %s \n", grade1);
+        System.out.printf("The Grade1 value is %c \n", grade1);
+        System.out.println("The Grade1 value is "+ grade1);
+
+    }
+}
