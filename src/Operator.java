9fe5d7449e5a6b0293465f36cef19975228447db
Ratul Nandy <ratul.tester@gmail.com>
2025-07-05 23:03:28 +0530

    files added

---
+++ src/Operator.java
@@ -0,0 +1,16 @@
+public class Operator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = b+a;
+        int d = b-a;
+        int e = b*a;
+        int f = b/a;
+        int g = b%a;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
---
+++ src/Unicode.java
@@ -0,0 +1,49 @@
+public class Unicode {
+    public static void main(String[] args) {
+        char unicode0 = '\u0F62';
+        char unicode1 = '\u1F62';
+        char unicode2 = '\u2F62';
+        char unicode3 = '\u3F62';
+        char unicode4 = '\u4F62';
+        char unicode5 = '\u5F62';
+        char unicode6 = '\u6F62';
+        char unicode7 = '\u7F62';
+        char unicode8 = '\u8F62';
+        char unicode9 = '\u9F62';
+        char unicode10 = '\u0F63';
+        char unicodeA = '\u0041';
+        char unicodeB = '\u0042';
+        char unicodeC = '\u0043';
+        char unicodeD = '\u0044';
+        char unicodeE = '\u0045';
+        char unicodeF = '\u0046';
+        char unicodeG = '\u0047';
+        char unicodeH = '\u0048';
+        char unicodeI = '\u0049';
+        char unicodeJ = '\u004A';
+
+        System.out.print(unicode0);
+        System.out.print(unicode1);
+        System.out.print(unicode2);
+        System.out.print(unicode3);
+        System.out.print(unicode4);
+        System.out.print(unicode5);
+        System.out.print(unicode6);
+        System.out.print(unicode7);
+        System.out.print(unicode8);
+        System.out.print(unicode9);
+        System.out.println(unicode10);
+        System.out.println("----------------------------------");
+        System.out.print(unicodeA);
+        System.out.print(unicodeB);
+        System.out.print(unicodeC);
+        System.out.print(unicodeD);
+        System.out.print(unicodeE);
+        System.out.print(unicodeF);
+        System.out.print(unicodeG);
+        System.out.print(unicodeH);
+        System.out.print(unicodeI);
+        System.out.print(unicodeJ);
+
+    }
+}
