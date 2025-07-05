de04e83da6e0f95bbc66cc7cc7287f1fc34bac8c
Ratul Nandy <ratul.tester@gmail.com>
2025-07-05 18:57:36 +0530

    Final file added

---
+++ src/Final_Setting.java
@@ -0,0 +1,14 @@
+public class Final_Setting {
+    public static void main(String[] args) {
+        final byte byteage = 123;
+        final int intage = 124;
+        final short shortage = 125;
+        final double floatage = 125.50;
+        //age = 124;
+        System.out.println(byteage);
+        System.out.println(intage);
+        System.out.println(shortage);
+        System.out.println(floatage);
+
+    }
+}
