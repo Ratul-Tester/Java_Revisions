19c4d5c303c5ce41cc467a2f70cc41bcc4482557
Ratul Nandy <ratul.tester@gmail.com>
2025-07-17 20:20:13 +0530

    added

---
+++ src/Practice3/Terenary.java
@@ -0,0 +1,12 @@
+package Practice3;
+
+public class Terenary {
+    public static void main(String[] args) {
+        int a = true ? 10 : 20;
+        int a1 = (30>40) ? 10 : 20;
+        System.out.println(a1);
+
+        String str= 10 > 20 ? "10" : "TWENTY";
+        System.out.println(str);
+    }
+}
