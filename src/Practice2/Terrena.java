8e32608141c0040f3f2851ef814e6958617458b6
Ratul Nandy <ratul.tester@gmail.com>
2025-07-17 16:01:59 +0530

    Terraynary added

---
+++ src/Practice2/Terrena.java
@@ -0,0 +1,11 @@
+package Practice2;
+
+public class Terrena {
+    public static void main(String[] args) {
+        int Ratul = 60, Ravi=40;
+        String result1 = Ratul>=60 ? "First Division" : "Second Division";
+        String result2 = Ravi>=50 ? "Second Division" : "Third Division";
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+}
