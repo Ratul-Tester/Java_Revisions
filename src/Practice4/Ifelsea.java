cab9995f2eb0354c1065a66ed590081911a1c6a2
Ratul Nandy <ratul.tester@gmail.com>
2025-07-18 07:43:56 +0530

    added

---
+++ src/Practice4/Ifelsea.java
@@ -0,0 +1,26 @@
+package Practice4;
+
+public class Ifelsea {
+    public static void main(String[] args) {
+        System.out.println("If Else program");
+        int age = 17, years = 18;
+        if (age >= 18){
+            System.out.println("You are eligible to watch movie");
+    }else{
+            System.out.println("You are NOT eligible to watch movie");
+        }
+
+        if (years == 17){
+            System.out.println("Hi");
+        }else {
+            System.out.println("Bye");
+        }
+        boolean b = !true;
+        if (b){
+            System.out.println("b is true");
+        }
+        else{
+            System.out.println("b is false");
+        }
+    }
+}
