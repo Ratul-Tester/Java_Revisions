20983ee2b90b029e6e28a241ce5ae42bc1065e61
Ratul Nandy <ratul.tester@gmail.com>
2025-07-29 20:12:54 +0530

    modified

--- src/Practice4/IfLoopLar3.java
+++ src/Practice4/IfLoopLar3.java
@@ -13,7 +13,9 @@ public class IfLoopLar3 {
             System.out.println(+num1 +" is greater");
         } else if (num2>num1 && num2>num3){
             System.out.println(+num2 +" is greater");
-        } else if (num1==num2 && num2==num3) {
+        } /*else if (num1) {
+
+        }*/ else if (num1==num2 || num2==num3) {
             System.out.println("Duplicate numbers identified");
         } else{
             System.out.println(+num3 +" is greater");
