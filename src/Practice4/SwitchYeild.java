4124e287825a2bb604f0534e004b7bb6757fb4e8
Ratul Nandy <ratul.tester@gmail.com>
2025-07-29 20:45:34 +0530

    added

--- src/Practice4/SwitchYeild.java
+++ src/Practice4/SwitchYeild.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class SwitchYeild {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         System.out.println("Enter a character to continue");
         char code = sc.next().charAt(0);
         int value = switch (code){
@@ -13,6 +13,16 @@ public class SwitchYeild {
             case 'A':
                 yield 65;
             default: //System.out.println("None");
+                throw  new IllegalStateException("Error");
+        };*/
+        char code = 'A';
+        int val = switch (code){
+            case 'A':
+                yield 65;
+            case 'B':
+                yield 66;
+            default:
+                System.out.println("Not able to find");
                 throw  new IllegalStateException("Error");
         };
     }
