d5e9552c4c2a33ba2ebf4e10fc1a67372924dd8b
Ratul Nandy <ratul.tester@gmail.com>
2025-08-06 16:10:57 +0530

    added

---
+++ src/ThizzFizz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ThizzFizz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the multiplication number for which it will be display Thizz:");
+        int Thizz = sc.nextInt();
+        System.out.println("Input the multiplication number for which it will be display Fizz:");
+        int Fizz = sc.nextInt();
+
+        for (int i=0;i<=100;i++){
+            if (i%Thizz==0 && i%Fizz==0 ){
+                System.out.println("ThizzFizz");
+            } else if (i%Thizz == 0) {
+                System.out.println("Thizz");
+            } else if (i%Fizz == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
