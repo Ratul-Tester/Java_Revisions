c385ed853d31df0e780fd055727a83a8cc5a510d
Ratul Nandy <ratul.tester@gmail.com>
2025-08-12 11:43:59 +0530

    added

---
+++ src/Practice7/BasicArray.java
@@ -0,0 +1,32 @@
+package Practice7;
+
+public class BasicArray {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5};
+        System.out.print(nums[4]);
+        System.out.print(" "+nums[3]);
+        System.out.print(" "+nums[2]);
+        System.out.print(" "+nums[1]);
+        System.out.print(" "+nums[0]+"\n");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        String[] names = {"Isqu","Visk","Pyar","Vyar"};
+        System.out.print(names[3]);
+        System.out.print(" "+names[2]);
+        System.out.print(" "+names[1]);
+        System.out.print(" "+names[0]+"\n");
+        System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+        int[] ages = new int[5];
+        ages[0] = 1;
+        ages[1] = 2;
+        ages[2] = 3;
+        ages[3] = 4;
+        ages[4] = 5;
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print(ages[i]+" ");
+        }
+        System.out.println("\nxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+
+
+    }
+}
