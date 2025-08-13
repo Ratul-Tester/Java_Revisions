60ec238be315570643c6975327a54ea97e0091c6
Ratul Nandy <ratul.tester@gmail.com>
2025-08-13 20:23:38 +0530

    added

--- src/Practice7/ArrayAllBasics.java
+++ src/Practice7/ArrayAllBasics.java
@@ -10,12 +10,16 @@ public class ArrayAllBasics {
         float[] floatArray = {12.32f,32.12f,13.23f};
         double[] doubleArray = {12.32,32.12,13.23};
         char[] charArray = {'A','a','B'};
+        String bal = charArray.toString();
         boolean[] booleanArray = {true,false,true};
         String[] stringArray = {"Jai","Shree","Krishna"};
-        String str1= "Ramesh";
+        String str1= "Ramesh Rao";
 
         System.out.println(byteArray.length);
         System.out.println(intArray.equals(longArray));
-        //System.out.println(Arrays.equals(intArray, longArray));
+        System.out.println(floatArray.equals(doubleArray));
+        System.out.println(charArray.toString());
+        System.out.println(bal);
+        System.out.println(str1.length());
     }
 }
