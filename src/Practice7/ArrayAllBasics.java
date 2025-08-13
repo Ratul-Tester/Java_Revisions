83fae6a5c5c1bd90effb11f8491b7d8bcc448962
Ratul Nandy <ratul.tester@gmail.com>
2025-08-13 16:54:09 +0530

    added

---
+++ src/Practice7/ArrayAllBasics.java
@@ -0,0 +1,21 @@
+package Practice7;
+
+import java.util.Arrays;
+
+public class ArrayAllBasics {
+    public static void main(String[] args) {
+        byte[] byteArray = {-127,0,127};
+        int[] intArray = {123,246,143};
+        long[] longArray = {123,246,143};
+        float[] floatArray = {12.32f,32.12f,13.23f};
+        double[] doubleArray = {12.32,32.12,13.23};
+        char[] charArray = {'A','a','B'};
+        boolean[] booleanArray = {true,false,true};
+        String[] stringArray = {"Jai","Shree","Krishna"};
+        String str1= "Ramesh";
+
+        System.out.println(byteArray.length);
+        System.out.println(intArray.equals(longArray));
+        //System.out.println(Arrays.equals(intArray, longArray));
+    }
+}
