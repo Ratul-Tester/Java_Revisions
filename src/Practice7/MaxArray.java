5aef36609367131b193e3d0ed08174a6990c3015
Ratul Nandy <ratul.tester@gmail.com>
2025-08-19 20:08:44 +0530

    added

--- src/Practice7/MaxArray.java
+++ src/Practice7/MaxArray.java
@@ -1,9 +1,20 @@
 package Practice7;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 
 public class MaxArray {
     public static void main(String[] args) {
         // Find the max from the array list
+        int[] nums = {2,6,9,8,7};
+        OptionalInt mi = Arrays.stream(nums).min();
+        System.out.println(mi+" is minimum");
+        int min = mi.getAsInt();
+        System.out.println(min+" is minimum");
+        OptionalInt ma = Arrays.stream(nums).max();
+        int max = ma.getAsInt();
+        System.out.println(ma+" is maximum");
+        System.out.println(max+" is maximum");
+
     }
 }
