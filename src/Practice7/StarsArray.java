9eaefa0a50a6bc7dbc9c0d1f3b64c1527ba8512e
Ratul Nandy <ratul.tester@gmail.com>
2025-08-22 12:42:40 +0530

    Right Angle Triangle

---
+++ src/Practice7/StarsArray.java
@@ -0,0 +1,13 @@
+package Practice7;
+
+public class StarsArray {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
