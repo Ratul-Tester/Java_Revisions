a5c17ce1561c6ccc143e22b90339dcddf98b46cf
Ratul Nandy <ratul.tester@gmail.com>
2025-09-01 17:01:55 +0530

    added

---
+++ src/Practice9/StaticAndNonStaticCall.java
@@ -0,0 +1,10 @@
+package Practice9;
+
+public class StaticAndNonStaticCall {
+    int x = 10;
+
+    public static void main(String[] args) {
+        StaticAndNonStaticCall t = new StaticAndNonStaticCall();
+        System.out.println(t.x);
+    }
+}
