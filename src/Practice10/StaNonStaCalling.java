f79c7c4237e63f3bf7123ae27fcdf183e1c76023
Ratul Nandy <ratul.tester@gmail.com>
2025-09-01 20:42:12 +0530

    added

---
+++ src/Practice10/StaNonStaCalling.java
@@ -0,0 +1,10 @@
+package Practice10;
+
+public class StaNonStaCalling {
+    int x =10;
+
+    public static void main(String[] args) {
+        StaNonStaCalling t = new StaNonStaCalling();
+        System.out.println(t.x);
+    }
+}
