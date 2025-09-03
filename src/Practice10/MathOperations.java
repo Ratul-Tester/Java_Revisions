8a3a4883509f4749f581c5818ce266be96a58ce9
Ratul Nandy <ratul.tester@gmail.com>
2025-09-03 16:35:21 +0530

    added

---
+++ src/Practice10/MathOperations.java
@@ -0,0 +1,15 @@
+package Practice10;
+
+import java.util.Scanner;
+
+public class MathOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the two numbers below");
+        System.out.println("Enter the first number (Num1):");
+        int Num1 = sc.nextInt();
+        System.out.println("Enter the second number (Num2):");
+        int Num2 = sc.nextInt();
+
+    }
+}
