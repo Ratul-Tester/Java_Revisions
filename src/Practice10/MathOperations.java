f1dcac71155f1f9f8d20493c71dfd2c2b6a35573
Ratul Nandy <ratul.tester@gmail.com>
2025-09-03 19:57:48 +0530

    added

--- src/Practice10/MathOperations.java
+++ src/Practice10/MathOperations.java
@@ -10,6 +10,35 @@ public class MathOperations {
         int Num1 = sc.nextInt();
         System.out.println("Enter the second number (Num2):");
         int Num2 = sc.nextInt();
+        System.out.println("Enter Operation Name like add,sub,mul,div for processing...");
+        String str = sc.next();
 
+        switch (str){
+            case "add": addition(Num1, Num2);
+                System.out.println(Num1+Num2);
+            break;
+            case "sub": substraction(Num1, Num2);
+                System.out.println(Num1-Num2);
+            break;
+            case "mul": multiplication(Num1, Num2);
+                System.out.println(Num1*Num2);
+            break;
+            case "div": division(Num1, Num2);
+                System.out.println(Num1/Num2);
+            break;
+        }
+
+    }
+    static int division(int Num1, int Num2) {
+        return Num1/Num2;
+    }
+    static int multiplication(int Num1, int Num2) {
+        return Num1*Num2;
+    }
+    static int substraction(int Num1, int Num2) {
+        return Num1-Num2;
+    }
+    static int addition(int Num1, int Num2) {
+        return Num1+Num2;
     }
 }
