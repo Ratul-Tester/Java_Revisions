ea10f8949f2204b49002f1e489a52edad081b6b3
Ratul Nandy <ratul.tester@gmail.com>
2025-09-10 16:48:49 +0530

    added

---
+++ src/Practice11/Dog.java
@@ -0,0 +1,21 @@
+package Practice11;
+
+public class Dog {
+    String name;
+    String breed;
+    String color;
+    int lifeSpan;
+
+    void diet(){
+
+    }
+    void biteForce(){
+        System.out.println("Heavy force");
+    }
+    void speed(){
+        System.out.println("0-40 in seconds");
+    }
+    void barkDB(){
+        System.out.println("Almost 230 DB");
+    }
+}
