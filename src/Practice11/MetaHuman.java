0276ca41cb7ceb92541d70212b0adecc014fe4e2
Ratul Nandy <ratul.tester@gmail.com>
2025-09-10 19:51:35 +0530

    added

---
+++ src/Practice11/MetaHuman.java
@@ -0,0 +1,22 @@
+package Practice11;
+
+public class MetaHuman {
+    // Atributes
+    String species;
+    String Class;
+    String name;
+    int age;
+    float height;
+    String gender;
+    String DOB;
+    boolean isMale;
+    String[] address;
+
+    //behaviour or functions
+    String species(){
+        return n;
+    }
+    void Class (){
+
+    }
+}
