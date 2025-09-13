65e51e460ce096edca361481e7a16c2c94cbc23f
Ratul Nandy <ratul.tester@gmail.com>
2025-09-13 09:19:33 +0530

    ade

---
+++ src/Oops/ATBenrolled.java
@@ -0,0 +1,18 @@
+package Oops;
+
+public class ATBenrolled {
+    //Below are the Instance Variables
+    String name;
+    String email;
+    long phoneNo;
+    String courseName;
+    boolean isEnrolled;
+
+    void printingDetails(){
+        System.out.println("Printing the details of enrolled students");
+    }
+
+    ATBenrolled(){
+        System.out.println("Default Constructor");
+    }
+}
---
+++ src/Oops/RunnerClass.java
@@ -0,0 +1,11 @@
+package Oops;
+
+public class RunnerClass {
+    public static void main(String[] args) {
+        ATBenrolled atb1 = new ATBenrolled();
+//        atb1.name = "Raty";
+//        System.out.println(atb1.name);
+
+    }
+
+}
