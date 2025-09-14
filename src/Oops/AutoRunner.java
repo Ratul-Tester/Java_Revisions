c66b716319271241290df900f74b286d7d83d8dd
Ratul Nandy <ratul.tester@gmail.com>
2025-09-14 10:51:10 +0530

    ads

---
+++ src/Oops/AutoRunner.java
@@ -0,0 +1,7 @@
+package Oops;
+
+public class AutoRunner {
+    public static void main(String[] args) {
+        Automation auto = new Automation();
+    }
+}
--- src/Oops/Automation.java
+++ src/Oops/Automation.java
@@ -7,6 +7,11 @@ public class Automation {
         excelOpen();
     }
 
+    public Automation(String name) {
+        this.name = name;
+        //System.out.println(name);
+    }
+
     void databaseConnection() {
         System.out.println("Database connection established");
     }
@@ -15,7 +20,28 @@ public class Automation {
         System.out.println("Excel opens for operation");
     }
 
+    void testCase1(){
+        System.out.println("test case 1 "+this.name);
+    }
+
+    void testCase2(){
+        System.out.println("test case 2 "+this.name);
+    }
+
     public static void main(String[] args) {
         Automation auto = new Automation();
+        //Automation auto1 = new Automation("Automation Started");
+        Automation t1 = new Automation("Test1");
+        Automation t2 = new Automation("Test2");
+
+        t1.databaseConnection();
+        System.out.println(t1.name);
+
+        t1.testCase1();
+        t1.testCase2();
+        System.out.println(t2.name);
+        t2.excelOpen();
+        t2.testCase1();
+        t2.testCase2();
     }
 }
