b3251a039b48fbcb04c74dfd80d6f2e3bb30f8aa
Ratul Nandy <ratul.tester@gmail.com>
2025-09-15 19:38:58 +0530

    afsg

--- src/Oops/BankAccount.java
+++ src/Oops/BankAccount.java
@@ -1,2 +1,30 @@
-package Oops;public class BankAccount {
+package Oops;
+
+public class BankAccount {
+    String BankName;
+    String BankCode;
+    long Balance;
+
+    BankAccount(){
+        BankName = "DefaultBankName";
+        BankCode = "XXOP000";
+        Balance = 0;
+    }
+
+    BankAccount(String bankName, String bankCode, long balance) {
+        BankName = bankName;
+        BankCode = bankCode;
+        Balance = balance;
+    }
+
+    BankAccount(String bankName, String bankCode) {
+        BankName = bankName;
+        BankCode = bankCode;
+    }
+
+    void PrintDetails(){
+        System.out.println("Bank Name is :"+BankName);
+        System.out.println("Bank Code is :"+BankCode);
+        System.out.println("Bank Balance is :"+Balance);
+    }
 }
--- src/Oops/BankRunner.java
+++ src/Oops/BankRunner.java
@@ -1,2 +1,17 @@
-package Oops;public class BankRunner {
+package Oops;
+
+public class BankRunner {
+    public static void main(String[] args) {
+        BankAccount Default = new BankAccount();
+        Default.PrintDetails();
+        System.out.println("**********************************");
+        BankAccount SBI = new BankAccount("SBI Bank","SBINC002",12000);
+        SBI.PrintDetails();
+        System.out.println("**********************************");
+        BankAccount HDFC = new BankAccount("HDFC Bank","IHDFC001",25600);
+        HDFC.PrintDetails();
+        System.out.println("**********************************");
+        BankAccount YES = new BankAccount("YES Bank","YESK009");
+        YES.PrintDetails();
+    }
 }
