3d78468c9e284dc66f98a598eb1c4ecd1f2927f6
Ratul Nandy <ratul.tester@gmail.com>
2025-09-17 16:58:35 +0530

    added

---
+++ src/Oops/StaffDetails.java
@@ -0,0 +1,10 @@
+package Oops;
+
+public class StaffDetails {
+    public static void main(String[] args) {
+        CBTSps Default = new CBTSps();
+        Default.printDetails();
+
+        CBTSps sl1 = new CBTSps("CBT","HOD","Prof. Sanghamitra Nayak","s.nayak@soa.ac.in",9080706050d);
+    }
+}
